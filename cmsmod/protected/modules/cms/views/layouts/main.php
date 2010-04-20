2d7344cd0a23c78dec1e51b9a759e273d0f1b606
balor <michal@balor.pl>
2010-04-20 20:48:06 +0200

    CMS module has its own css files from on now.

--- cmsmod/protected/modules/cms/views/layouts/main.php
+++ cmsmod/protected/modules/cms/views/layouts/main.php
@@ -5,14 +5,14 @@
 	<meta name="language" content="en" />
 
 	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->getModule('cms')->assets; ?>/screen.css" media="screen, projection" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->getModule('cms')->assets; ?>/print.css" media="print" />
 	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->getModule('cms')->assets; ?>/ie.css" media="screen, projection" />
 	<![endif]-->
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->getModule('cms')->assets; ?>/main.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->getModule('cms')->assets; ?>/form.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
